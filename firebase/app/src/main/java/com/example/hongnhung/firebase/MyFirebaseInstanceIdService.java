44ca70a0826119d6d236607de661d094c92f6b2f
Hong Nhung <hongnhung@Hongs-MacBook-Pro.local>
2017-03-03 13:17:58 +0700

    push code

---
+++ firebase/app/src/main/java/com/example/hongnhung/firebase/MyFirebaseInstanceIdService.java
@@ -0,0 +1,25 @@
+package com.example.hongnhung.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by hongnhung on 03/03/2017.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("","Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+
+    }
+
+}
---
+++ firebase/app/src/main/java/com/example/hongnhung/firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,90 @@
+package com.example.hongnhung.firebase;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by hongnhung on 03/03/2017.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        // Check if message contains a data payload.
+
+        if (remoteMessage.getData().size() > 0) {
+            Log.d("", "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d("", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        if (remoteMessage.getNotification() != null)
+            sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody(), 0);
+        else {
+            String title = getString(R.string.app_name);
+            String body = "";
+
+
+
+        }
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private void sendNotification(String title, String messageBody, int surveyId) {
+        Intent intent;
+                    intent = new Intent(this, MainActivity.class);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+                .setDefaults(Notification.DEFAULT_ALL)
+            /*
+     * Sets the big view "big text" style and supplies the
+     * text (the user's reminder message) that will be displayed
+     * in the detail area of the expanded notification.
+     * These calls are ignored by the support library for
+     * pre-4.1 devices.
+     */
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(messageBody));
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+}
+
